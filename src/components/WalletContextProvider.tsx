baee237ec991953513d26f130ea4c90d19751d4b
jeffreyon <jeffreyon11@gmail.com>
2024-05-22 12:03:31 +0100

    feat: added explicit wallets

--- src/components/WalletContextProvider.tsx
+++ src/components/WalletContextProvider.tsx
@@ -5,11 +5,18 @@ import {
 } from "@solana/wallet-adapter-react";
 import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
 import { Cluster, clusterApiUrl } from "@solana/web3.js";
+import {
+  PhantomWalletAdapter,
+  SolflareWalletAdapter,
+} from "@solana/wallet-adapter-wallets";
 require("@solana/wallet-adapter-react-ui/styles.css");
 
 const WalletContextProvider: FC<{ children: ReactNode }> = ({ children }) => {
   const network = clusterApiUrl(process.env.NEXT_PUBLIC_CLUSTER as Cluster);
-  const wallets = useMemo(() => [], []);
+  const wallets = useMemo(
+    () => [new PhantomWalletAdapter(), new SolflareWalletAdapter()],
+    [network]
+  );
 
   return (
     <ConnectionProvider endpoint={network}>
