d82f72fed96831ca264ae77550c5f6b92c0cc625
jeffreyon <jeffreyon11@gmail.com>
2024-06-24 16:59:20 +0100

    fix: remote livestream context proivder imports

--- src/components/ViewerControls.tsx
+++ src/components/ViewerControls.tsx
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import { useMusicControl } from "./MusicControlContext";
 import { useDispatch, useSelector } from "react-redux";
 import {
   getAccessToken,
